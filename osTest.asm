e0a929bab04d99606d85f0c84917d4b11d83e770
Valaki <fekete.kocsis500@gmail.com>
2022-09-30 12:15:15 +0200

    final

--- osTest.asm
+++ osTest.asm
@@ -12,25 +12,29 @@ backToLoop: ;if al contains ` prints out the buffer otherwise pushes into the st
     push ax
     jmp looop
 
-waitForKeypress:
+waitForKeypress:    ;waits for keypress
     mov ah, 0
     int 0x16
     jmp backToLoop
 
-writeBuffer:
+writeBuffer:    ;writes the buffer
     mov ah, 0x0e
+    cmp bp, sp
+    je beforeRepeat
     mov bx, bp
     jmp writeLoop
 
-writeLoop:
+writeLoop:  ;writes a single character from the buffer
     mov al, [bx - 2]
     int 0x10
+    mov cx, 0
+    mov [bx - 2], cx
     sub bx, 2
     cmp bx, sp
     je beforeRepeat
     jmp writeLoop
 
-beforeRepeat:
+beforeRepeat:   ;writes enter
     mov al, 0ah
     int 0x10
     mov al, 0dh
